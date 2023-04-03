0db2e5d7f1b1917cecc43243577a36e79874c72b
ankushjana29 <73129465+ankushjana29@users.noreply.github.com>
2023-04-03 17:12:18 +0530

    Create Spectrogram.py

---
+++ Spectrogram/Spectrogram.py
@@ -0,0 +1,14 @@
+import librosa as lr
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+from librosa.display import specshow
+
+audio=("/home/audio/Asian_Koel.wav")
+x,sr=lr.load(audio)
+
+X = lr.stft(x)
+Xdb = lr.amplitude_to_db(abs(X))
+plt.figure(figsize = (10, 5))
+lr.display.specshow(Xdb, sr = sr, x_axis = 'time', y_axis = 'hz')
+plt.colorbar()
+
