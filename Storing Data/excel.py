ddc1a2add5c21602ca224ce81c8ef4a64c3b5baf
ankushjana29 <73129465+ankushjana29@users.noreply.github.com>
2023-04-12 23:34:25 +0530

    Takes hyperparameters as input and saves the spectral feature values in excel

--- Storing Data/excel.py
+++ Storing Data/excel.py
@@ -8,30 +8,42 @@ import numpy as np
 print("Enter the path:")
 path = input()
 
+frame_size = int(input("Enter frame size (in samples): "))
+hop_length = int(input("Enter hop length (in samples): "))
+
 # create empty lists to store results
 spectral_centroids = []
 rolloffs = []
 spectral_bandwidths = []
+fft_a=[]
+mean_spec_cent=[]
 filenames = []
 
+count=0
 # accessing audio files in folder
 for filename in os.listdir(path):
     if filename.endswith('.wav'): # only process .wav files
         audio_path = os.path.join(path, filename)
-        y, sr = librosa.load(audio_path,sr=None) # load audio file
-        centroid = librosa.feature.spectral_centroid(y=y, sr=sr)[0]
-        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)[0]
-        bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)[0]
-        spectral_centroids.append(centroid.mean()) # store mean spectral centroid
+        y, sr = librosa.load(audio_path) # load audio file
+        centroid = librosa.feature.spectral_centroid(y=y, sr=sr, n_fft=frame_size, hop_length=hop_length)[0]
+        mean_spec_cent = centroid.mean() 
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr, n_fft=frame_size, hop_length=hop_length)[0]
+        fft_data = np.fft.fft(y)
+        fft_abs = np.abs(fft_data)
+        fft_a.append(fft_abs.mean())
+        bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr, n_fft=frame_size, hop_length=hop_length)[0]
+        spectral_centroids.append(mean_spec_cent) # store mean spectral centroid
         rolloffs.append(rolloff.mean()) # store mean spectral rolloff
         spectral_bandwidths.append(bandwidth.mean())
         filenames.append(filename) # store filename
+        count+=1    
         
 # create a pandas DataFrame to store results
 df = pd.DataFrame({'filename': filenames,
                    'spectral_centroid': spectral_centroids,
                    'spectral_bandwidth': spectral_bandwidths,
-                   'rolloff': rolloffs})
+                   'rolloff': rolloffs,
+                   'FFT': fft_a})
 
 # specify the path to the folder where you want to save the Excel file
 print("Enter the path where you want to save:")
@@ -44,7 +56,10 @@ excel_file_path = os.path.join(excel_folder_path, 'audio_features.xlsx')
 wb = Workbook()
 ws = wb.active
 ws.title = "Audio Features"
-ws.append(['filename', 'spectral_centroid', 'rolloff','spectral_bandwidth'])
+ws.append(['filename', 'spectral_centroid', 'rolloff','spectral_bandwidth','FFT'])
 for index, row in df.iterrows():
-    ws.append([row['filename'], row['spectral_centroid'], row['rolloff'],row['spectral_bandwidth']])
+    ws.append([row['filename'], row['spectral_centroid'], row['rolloff'],row['spectral_bandwidth'],row['FFT']])
 wb.save(excel_file_path)
+
+print("Total number of files processed: ", count)
+
